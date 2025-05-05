87acfcf1fc44296cf950b9a883795859084c4e12
Qianhe Xiao <lapis_xiao@outlook.com>
2025-05-05 09:49:19 +0800

    hw3 completed

--- src/components/BadgerMart.jsx
+++ src/components/BadgerMart.jsx
@@ -4,9 +4,18 @@ import FeaturedItem from "./FeaturedItem";
 export default function BadgerMart(props) {
 
     const [feature, setFeature] = useState();
+    
 
     useEffect(() => {
         // TODO I should fetch and setFeature here!
+        fetch("https://cs571.org/rest/s25/hw3/featured-item", {
+            headers: {"X-CS571-ID": "bid_0dc79fe52f6a30a5bfdd920f25f4b01da3803f43cce3d87dfb17406e23676d1a"} 
+        })
+        .then(response => response.json())
+        .then(data => {
+            setFeature(data);
+            console.log(data);
+        })
     }, []);
 
     return <div>
@@ -15,6 +24,6 @@ export default function BadgerMart(props) {
             <p>BadgerMart, located at the heart of the UW-Madison campus, is the go-to grocery store for students, faculty, and staff. Offering a wide range of products, including fresh produce, dairy, bakery items, meat, seafood, and pantry essentials, BadgerMart ensures that the university community has access to everything they need. With exclusive student discounts, convenient online ordering, and meal kits designed for busy schedules, shopping at BadgerMart is both affordable and effortless.</p>
         </div>
         <h2>Today's Special</h2>
-        {feature ? <FeaturedItem {...feature}/> : <p>Today's feature is still loading...</p>}
+        {feature ? <FeaturedItem {...feature}/> : <p>Today's feature is still loading...</p>}  
     </div>
 }
--- src/components/FeaturedItem.jsx
+++ src/components/FeaturedItem.jsx
@@ -1,5 +1,38 @@
+import React, { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+
 export default function FeaturedItem(props) {
-    return <div>
-        <p>I should display the feature that was passed to me...</p>
-    </div>
+    const [showNutrition, setShowNutrition] = useState(false);
+
+    return <Card>
+        <img src={props.img} alt={props.name}></img>
+        <h3><strong>{props.name}</strong></h3>
+        <h4><strong>${props.price} per unit</strong></h4>
+        <p>{props.description}</p> 
+        {showNutrition && <p>Nutrition Facts</p>}
+        {showNutrition &&
+        <Table>
+            <thead> 
+                <tr>
+                    <td>Calories</td>
+                    <td>Fat</td>
+                    <td>Carbohydrates</td>
+                    <td>Protein</td>
+                </tr>
+            </thead>
+            <tbody>
+                
+                <tr>
+                    <td>{props.nutrition.calories ? props.nutrition.calories : 0}</td>
+                    <td>{props.nutrition.fat ? props.nutrition.fat : 0}</td>
+                    <td>{props.nutrition.carbohydrates ? props.nutrition.carbohydrates : 0}</td>
+                    <td>{props.nutrition.protein ? props.nutrition.protein : 0}</td>
+                </tr>
+            </tbody>
+        </Table>
+        } 
+        <Button onClick={() => setShowNutrition(!showNutrition)}>{showNutrition ? "Hide Nutrition" : "Show Nutrition"}</Button>
+    </Card>
 }
